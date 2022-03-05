72255b88b6baeba1833686082581da123bde5ef4
achmad anshori <utd.rik@gmail.com>
2022-03-06 06:59:37 +0700

    fix cors

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-require("dotenv").config();
-const express = require("express");
-const morgan = require("morgan");
-const helmet = require("helmet");
-const mainRouter = require("./src/routers/main");
-const cors = require("cors");
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const mainRouter = require('./src/routers/main');
+const cors = require('cors');
 
 const server = express();
 const logger = morgan(
-  ":method :url :status :res[content-length] - :response-time ms"
+  ':method :url :status :res[content-length] - :response-time ms'
 );
 
 const port = process.env.PORT || 8080;
@@ -17,19 +17,19 @@ server.listen(port, () => {
 
 const corsOptions = {
   origin: [
-    "http://localhost:8080",
-    "http://localhost:3000",
-    "https://naughty-jepsen-5afced.netlify.app",
+    'http://localhost:8080',
+    'http://localhost:3000',
+    'https://vehicle-rental-reactjs.netlify.app',
   ],
-  allowedHeaders: ["x-access-token", "content-type"],
-  methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ['x-access-token', 'content-type'],
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
 };
 
 server.use(cors(corsOptions));
 
 server.use(helmet());
-server.use(express.urlencoded({ extended: true }));
+server.use(express.urlencoded({extended: true}));
 server.use(express.json());
 server.use(logger);
-server.use(express.static("public"));
+server.use(express.static('public'));
 server.use(mainRouter);
